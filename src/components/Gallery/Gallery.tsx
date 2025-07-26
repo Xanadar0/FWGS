f0875c09f48408a11bfc7429081e57504be29970
Xanadar0 <61867465+Xanadar0@users.noreply.github.com>
2025-07-26 19:17:54 +0300

    -

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -5,6 +5,7 @@ import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 import styles from "./Gallery.module.css";
+import images from '/src/data/imagesData.js';
 
 // If you want you can use SCSS instead of css
 import 'lightgallery/scss/lightgallery.scss';
@@ -13,19 +14,6 @@ import 'lightgallery/scss/lg-zoom.scss';
 // import plugins if you need
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
-
-const images = [
-    { src: '/Img/Gallery/1.png', alt: 'The main menu of CS16Client, running on an Android phone. CS16Client Screenshot' },
-    { src: '/Img/Gallery/2.png', alt: 'The Counter-Strike team selection menu, running on Android. CS16Client in-game Screenshot' },
-    { src: '/Img/Gallery/3.png', alt: 'The Counter-Strike class selection menu, running on Android. CS16Client in-game Screenshot' },
-    { src: '/Img/Gallery/4.png', alt: 'In-game screenshot of CS16Client, playing on the T team. CS16Client in-game Screenshot' },
-    { src: '/Img/Gallery/5.png', alt: 'A CT about to get sniped in Dust II. CS16Client in-game Screenshot' },
-    { src: '/Img/Gallery/6.png', alt: 'The main menu of Half-Life, running on an Android phone. Half-Life in-game Screenshot' },
-    { src: '/Img/Gallery/7.png', alt: 'A screenshot of the Half-Life singleplayer campaign, the player being assisted by Barney. Running on Android. Half-Life in-game Screenshot' },
-    { src: '/Img/Gallery/8.png', alt: 'Counter-Strike Dust map running on Linux 32-bit CS16Client. CS16Client in-game Screenshot' },
-    { src: '/Img/Gallery/9.png', alt: 'Screenshot of Quake running on Linux 32-bit Xash3D FWGS engine. Quake in-game Screenshot' },
-    { src: '/Img/Gallery/10.png', alt: 'Screenshot of Half-Life: Opposing Force running on Linux 32-bit Xash3D FWGS engine. Opposing Force in-game Screenshot' },
-]
 
 export default function Gallery() {
     const onInit = () => {
